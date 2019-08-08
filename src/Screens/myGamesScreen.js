13a7b8ca75da8f3fed1f7122c03c32998f612716
mombanger69 <hugiholm1@gmail.com>
2019-08-08 22:38:13 +0000

    game screen

---
+++ src/Screens/myGamesScreen.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+
+
+
+export default class myGamesScreen extends Component {
+    
+    render() {
+        return (
+        <View>
+          <Text>My games screen</Text>
+        </View>
+        );
+    }
+}
+
+
